157fe80d11c808071bdc275897212569677c9bc0
Tenn Chio <tenn_chio@icloud.com>
2019-11-15 23:06:01 +0800

    Update project structure.
    Initialize the project and add a spring boot web demo.

---
+++ spring-boot-demo/src/test/java/com/srcube/spring/boot/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srcube.spring.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
